88900ecc87dde32f859c6c4b5b38db7ab09b3a59
bhargavireddy2412 <135250639+bhargavireddy2412@users.noreply.github.com>
2026-04-13 03:32:09 +0100

    ncbncbnc

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("git rebase demooooooo");
-<<<<<<< HEAD
         System.out.println("master changes");
                 System.out.println("master changed second time");
     
-=======
         System.out.println("feature branch added");
->>>>>>> e5b4889 (feature changes)
+        System.out.println("master changed by another branch");
     }
 }
